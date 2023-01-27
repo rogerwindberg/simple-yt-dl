cb3aa4d891b70da67738d9bafb9fd620b91b40d7
Roger Windberg <rogerwindberg@hotmail.com>
2023-01-27 13:20:41 -0300

    first commit

---
+++ YT-DL (Advanced).bat
@@ -0,0 +1,59 @@
+@ECHO OFF
+cd /d %~dp0bin
+ECHO ======================================================================================================================
+ECHO.
+SET /P URL="[Enter video URL]: "
+ECHO.
+ECHO ======================================================================================================================
+goto formatList
+
+:formatList
+ECHO.
+youtube-dl -F %URL%
+ECHO.
+ECHO ======================================================================================================================
+goto selection
+
+
+:selection
+ECHO.
+ECHO 1) Video + Audio
+ECHO 2) Single format (Audio only / Video only)
+ECHO.
+SET /P option="Select option: "
+if %option% == 1 (goto download)
+if %option% == 2 (goto downloadSingle)
+ECHO.
+ECHO Unknown value
+ECHO.
+ECHO ======================================================================================================================
+goto selection
+
+:download
+ECHO.
+SET /P video="Select video format: "
+SET /P audio="Select audio format: "
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+youtube-dl -o ../Downloads/%%(title)s.%%(ext)s -f %video%+%audio% --merge-output-format mp4 "%URL%"
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+ECHO Done!
+PAUSE
+EXIT
+
+:downloadSingle
+ECHO.
+SET /P format="Select format: "
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+youtube-dl -o ../Downloads/%%(title)s.%%(ext)s -f %format% -i --ignore-config --hls-prefer-native %URL%
+ECHO.
+ECHO ======================================================================================================================
+ECHO.
+ECHO Done!
+PAUSE
+EXIT
